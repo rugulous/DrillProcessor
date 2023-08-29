aed20e24686bdae28a2936eb0ecc80fd8ae0e18c
Luke Taylor <luke@circleleasing.co.uk>
2023-08-29 16:47:51 +0100

    Draw sets

--- DrillProcessor/Program.cs
+++ DrillProcessor/Program.cs
@@ -1,6 +1,7 @@
 ﻿using DrillProcessor;
 using iText.Kernel.Pdf;
 using iText.Kernel.Pdf.Canvas.Parser;
+using System.Drawing;
 using System.Text;
 using System.Text.RegularExpressions;
 
@@ -55,17 +56,19 @@ Performer ExtractPerformer(string chunk)
             whereDoesDrillStartFrom = 1;
         }
 
-        if(i == lines.Length - 1)
+        if (i == lines.Length - 1)
         {
             //last line has no out / step size
             whereDoesDrillEnd = 0;
-        } else
+        }
+        else
         {
             int spacesToTrim = -1;
             if (parts[parts.Length - 1] == "Halt")
             {
                 spacesToTrim = 2;
-            } else
+            }
+            else
             {
                 spacesToTrim = 4; //24, 9 to 5
             }
@@ -85,7 +88,7 @@ Performer ExtractPerformer(string chunk)
 
 (string x, string y, decimal? x1, decimal? y1) ExtractCoords(string drillCoords)
 {
-    if(drillCoords == "You don't stop on this subset")
+    if (drillCoords == "You don't stop on this subset")
     {
         return (null, null, null, null);
     }
@@ -114,13 +117,15 @@ decimal CalculateXPos(Match match)
     int multiplier = 0;
     int marker = 0;
     decimal steps = 0;
-    if(staging == "Stage Left")
+    if (staging == "Stage Left")
     {
         multiplier = -1;
-    } else if(staging == "Stage Right")
+    }
+    else if (staging == "Stage Right")
     {
         multiplier = 1;
-    } else
+    }
+    else
     {
         //on 50
         multiplier = 0;
@@ -129,13 +134,14 @@ decimal CalculateXPos(Match match)
     if (coords[0] == "On")
     {
         marker = int.Parse(coords[1]); //On 50
-    } else
+    }
+    else
     {
         marker = int.Parse(coords[2]); //xx outside 50
         steps = decimal.Parse(coords[0]) * (coords[1] == "Outside" ? 1 : -1);
     }
 
-    marker = Math.Abs(50 - marker); //calc distance from 50: 40 = 10, 30 = 20, etc 
+    marker = Math.Abs(50 - marker) / 5; //calc distance from 50: 40 = 2, 30 = 4, etc 
     steps += marker * 8; //coordinate system sees 8 paces inbetween each marker
 
     return steps * multiplier;
@@ -161,20 +167,21 @@ decimal CalculateYPos(string coords)
     var position = match.Groups[1].Value;
     var hash = match.Groups[2].Value;
 
-    if(!hashes.ContainsKey(hash))
+    if (!hashes.ContainsKey(hash))
     {
         throw new Exception($"Couldn't recognise hash {hash}");
     }
 
     decimal steps = hashes[hash];
-    if(!position.StartsWith("On"))
+    if (!position.StartsWith("On"))
     {
         decimal posPart = decimal.Parse(coords.Split(" ")[0]);
 
-        if(position.Contains("In Front"))
+        if (position.Contains("In Front"))
         {
             steps -= posPart;
-        } else
+        }
+        else
         {
             steps += posPart;
         }
@@ -194,20 +201,45 @@ foreach (string chunk in performerChunks)
 }
 
 Console.WriteLine($"Found {performers.Count} performers");
-foreach(Performer performer in performers)
+foreach (Performer performer in performers)
 {
+    Bitmap bmp = new(1600, 906);
+    Graphics graphics = Graphics.FromImage(bmp);
+    graphics.FillRectangle(Brushes.DarkGreen, 0, 0, 1600, 53);
+    graphics.FillRectangle(Brushes.Green, 0, 53, 1600, 853);
+    graphics.FillRectangle(Brushes.White, 0, 53, 1600, 1);
+
+    for (int i = 0; i <= 20; i++)
+    {
+        graphics.FillRectangle(Brushes.White, i * 80, 53, (i == 10 ? 5 : 1), 853);
+        graphics.FillRectangle(Brushes.White, (i * 80) - 20, 373, 40, 1);
+        graphics.FillRectangle(Brushes.White, (i * 80) - 20, 586, 40, 1);
+
+    }
+
+    for (int j = 0; j < 530; j += 80)
+    {
+        graphics.FillEllipse(Brushes.White, 797, 48 + j, 10, 10);
+    }
+
     Console.WriteLine($"Performer {performer.Name} ({performer.Label})");
-    foreach(DrillSet set in performer.Sets)
+    foreach (DrillSet set in performer.Sets)
     {
         Console.Write($"  Set {set.Identifier}");
-        if(set.RehearsalMark != null)
+        if (set.RehearsalMark != null)
         {
             Console.Write($" (Rehearsal Mark {set.RehearsalMark})");
         }
 
         Console.WriteLine($": {set.rawCoordsX}, {set.rawCoordsY} with {set.CountsToNextSet} to move || {set.X}, {set.Y}");
+
+        if (set.X != null && set.Y != null)
+        {
+            graphics.FillEllipse(Brushes.Black, 800 + ((int)set.X * 10) - 3, 53 + ((int)set.Y * 10) - 3, 10, 10);
+        }
     }
 
+    bmp.Save($"C:\\temp\\drill\\{performer.Label}.bmp");
     Console.WriteLine();
     Console.WriteLine();
 }
