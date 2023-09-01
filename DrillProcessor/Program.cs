75b83825352e9d0361acf21c5d217cb21351c3f2
Luke Taylor <luke@circleleasing.co.uk>
2023-09-01 13:26:34 +0100

    Export all of em

--- DrillProcessor/Program.cs
+++ DrillProcessor/Program.cs
@@ -1,29 +1,10 @@
 ﻿using DrillProcessor;
 using System.Drawing;
 
-List<RawPerformer> opener = DrillExtractor.Extract("Drill/P1.pdf");
+string[] movements = { "P1", "P2", "P3", "P4" };
 
-//List<RawPerformer> performers = DrillExtractor.Extract("Drill/P4.pdf", 
-//    DrillExtractor.Extract("Drill/P3.pdf", 
-//        DrillExtractor.Extract("Drill/P2.pdf", 
-//            DrillExtractor.Extract("Drill/P1.pdf")
-//        )
-//    )
-//);
-
-//DotPlotter plotter = new(100);
-//Performer matt = performers.FirstOrDefault(p => p.Label == "S2");
-//Bitmap? dots = plotter.PlotDots(matt);
-//dots?.Save("C:\\temp\\drill\\Matt.bmp");
-//foreach (Performer performer in performers)
-//{
-//    using Bitmap? bmp = plotter.PlotDots(performer);
-//    if (OperatingSystem.IsWindows())
-//    {
-//        bmp?.Save($"C:\\temp\\drill\\{performer.Label}.bmp");
-//    }
-//}
-
-//DotExporter.Export(opener.First(p => p.Label == "S2"), "C:\\temp\\drill\\matt.txt");
-
-DotExporter.Export(opener, "C:\\temp\\drill\\all.drill");
+for(int i = 0; i < movements.Length; i++)
+{
+    List<RawPerformer> performers = DrillExtractor.Extract($"Drill/{movements[i]}.pdf");
+    DotExporter.Export(performers, $"C:\\temp\\drill\\{movements[i]}.drill");
+}
