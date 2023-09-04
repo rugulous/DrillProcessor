ab39056e6df7be52c783bae88a2b6d4afece55a6
Luke Taylor <luke@circleleasing.co.uk>
2023-09-04 14:20:38 +0100

    Add subset calculations

--- DrillProcessor/DotExporter.cs
+++ DrillProcessor/DotExporter.cs
@@ -6,9 +6,9 @@
 
         public static void Export(List<RawPerformer> performers, string fileTo)
         {
-            if(performers.Count == 0) return;
+            if (performers.Count == 0) return;
 
-            if(File.Exists(fileTo))
+            if (File.Exists(fileTo))
             {
                 File.Delete(fileTo);
             }
@@ -20,7 +20,7 @@
                 writer.WriteLine(performance.Title);
                 writer.WriteLine(performance.Bpm);
 
-                foreach(PerformanceSet set in performance.Sets)
+                foreach (PerformanceSet set in performance.Sets)
                 {
                     writer.WriteLine(set.Identifier);
                     writer.WriteLine(set.CountsToNextSet ?? 0);
@@ -28,34 +28,18 @@
 
                 writer.WriteLine(TERMINATOR);
 
-                foreach(Performer performer in performance.Performers)
+                foreach (Performer performer in performance.Performers)
                 {
                     writer.WriteLine(performer.Name);
                     writer.WriteLine(performer.Symbol);
 
-                    foreach(Dot dot in performer.Dots)
+                    foreach (Dot dot in performer.Dots)
                     {
                         writer.WriteLine($"{dot.X},{dot.Y}");
                     }
 
                     writer.WriteLine(TERMINATOR);
                 }
-
-                //writer.WriteLine(performer.Name);
-                //writer.WriteLine(performer.Label);
-
-                //foreach(DrillSet set in performer.Sets)
-                //{
-                //    writer.WriteLine(set.Identifier);
-                //    writer.WriteLine(set.CountsToNextSet ?? 0);
-                //    if(set.X == null || set.Y == null)
-                //    {
-                //        writer.WriteLine("-");
-                //    } else
-                //    {
-                //        writer.WriteLine($"{set.X},{set.Y}");
-                //    }
-                //}
             }
         }
 
@@ -64,7 +48,7 @@
             Performance performance = new("Test Drill", 180);
 
             RawPerformer showTemplate = performers[0];
-            foreach(DrillSet drillSet in showTemplate.Sets)
+            foreach (DrillSet drillSet in showTemplate.Sets)
             {
                 PerformanceSet set = new()
                 {
@@ -77,14 +61,80 @@
                 performance.Sets.Add(set);
             }
 
-            foreach(RawPerformer performer in performers)
+            foreach (RawPerformer performer in performers)
             {
                 Performer p = new(performer.Name, performer.Label);
-                p.Dots = performer.Sets.ConvertAll(s => new Dot()
+
+                DrillSet? subsetStart = null;
+                DrillSet? subsetEnd = null;
+                decimal subsetTotal = 0;
+                decimal subsetProgress = 0;
+                decimal totalMoveX = 0;
+                decimal totalMoveY = 0;
+
+                for (int i = 0; i < performer.Sets.Count; i++)
                 {
-                    X = s.X,
-                    Y = s.Y
-                });
+                    Dot dot = null;
+                    if (performer.Sets[i].X == null || performer.Sets[i].Y == null)
+                    {
+                        //great, we're in a subset
+                        //do we have the next set?
+
+                        Console.WriteLine($"Detected subset for {performer.Name} at set {performer.Sets[i].Identifier}");
+
+                        if(subsetStart == null)
+                        {
+                            subsetStart = performer.Sets[i - 1];
+                            subsetTotal = 0;
+                        }
+
+                        if (subsetEnd == null)
+                        {
+                            Console.WriteLine("We don't know where the subset ends :(");
+                            //where's the next set?
+                            subsetTotal += performer.Sets[i].CountsToNextSet ?? 0;
+
+
+                            for (int j = i + 1; j < performer.Sets.Count; j++)
+                            {
+                                subsetTotal += performer.Sets[j].CountsToNextSet ?? 0;
+
+                                if (performer.Sets[j].X != null && performer.Sets[j].Y != null)
+                                {
+                                    subsetEnd = performer.Sets[j];
+                                    Console.WriteLine($"Subset ends at {subsetEnd.Identifier} - {subsetTotal} counts later  - ({subsetEnd.X}, {subsetEnd.Y})");
+                                    break;
+                                }
+                            }
+
+                            subsetProgress = performer.Sets[i].CountsFromLastSet ?? 0;
+                            totalMoveX = (subsetStart.X!.Value - subsetEnd.X.Value);
+                            totalMoveY = (subsetStart.Y!.Value - subsetEnd.Y.Value);
+                        }
+
+                        decimal progress = (subsetProgress / subsetTotal);
+
+
+                        dot = new()
+                        {
+                            X = subsetStart.X!.Value - (totalMoveX * progress),
+                            Y = subsetStart.Y!.Value - (totalMoveY * progress)
+                        };
+                    }
+                    else
+                    {
+                        subsetStart = null;
+                        subsetEnd = null;
+
+                        dot = new()
+                        {
+                            X = performer.Sets[i].X,
+                            Y = performer.Sets[i].Y
+                        };
+                    }
+
+                    p.Dots.Add(dot);
+                }
 
                 performance.Performers.Add(p);
             }
