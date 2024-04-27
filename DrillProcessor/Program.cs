df898362272c68cd511d4dfd7fd7c29e5a0c49f9
Luke Taylor <luke@circleleasing.co.uk>
2024-04-27 21:02:37 +0100

    Move Drill -> Drill/2023

--- DrillProcessor/Program.cs
+++ DrillProcessor/Program.cs
@@ -5,6 +5,6 @@ string[] movements = { "P1", "P2", "P3", "P4" };
 
 for(int i = 0; i < movements.Length; i++)
 {
-    List<RawPerformer> performers = DrillExtractor.Extract($"Drill/{movements[i]}.pdf");
+    List<RawPerformer> performers = DrillExtractor.Extract($"Drill/2023/{movements[i]}.pdf");
     DotExporter.Export(performers, $"C:\\temp\\drill\\{movements[i]}.drill");
 }
