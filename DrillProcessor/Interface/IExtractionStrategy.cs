265f2fa52976d6ee5e28225b7496ad66d06011a2
Luke Taylor <luke@circleleasing.co.uk>
2024-05-08 13:04:11 +0100

    Outline for generic extractor using strategy pattern (?)

---
+++ DrillProcessor/Interface/IExtractionStrategy.cs
@@ -0,0 +1,10 @@
+﻿using DrillProcessor.Model;
+
+namespace DrillProcessor.Interface
+{
+    internal interface IExtractionStrategy
+    {
+        public string[] PrepareChunks(string fileContents);
+        public RawPerformer ExtractPerformer(string[] lines);
+    }
+}
